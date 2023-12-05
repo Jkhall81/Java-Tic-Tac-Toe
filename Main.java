ecbe418ff3abd1bab75bb83f3e11a7a7b441465f
Jkhall81 <jason.kei.hall@gmail.com>
2023-12-05 16:10:46 -0700

    X changes to O and vice versa after each turn.  getting used to java syntax.  Seems like a wordier form of javascript that has to be in classes.

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main {
 
@@ -5,10 +6,27 @@ public class Main {
         
         char[] pos = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 
-        System.out.println(" " + pos[6] + " " + "|" + " " + pos[7] + " | " + pos[8] +  " ");
-        System.out.println("---+---+---");
-        System.out.println(" " + pos[3] + " " + "|" + " " + pos[4] + " | " + pos[5] +  " ");
-        System.out.println("---+---+---");
-        System.out.println(" " + pos[0] + " " + "|" + " " + pos[1] + " | " + pos[2] +  " ");
+        Scanner scanner = new Scanner(System.in);
+        int input;
+        char turn = 'X';
+
+        while (true) {
+            System.out.print("Enter a position: ");
+            input = scanner.nextInt();
+            pos[input - 1] = turn;
+
+            System.out.println(" " + pos[6] + " " + "|" + " " + pos[7] + " | " + pos[8] +  " ");
+            System.out.println("---+---+---");
+            System.out.println(" " + pos[3] + " " + "|" + " " + pos[4] + " | " + pos[5] +  " ");
+            System.out.println("---+---+---");
+            System.out.println(" " + pos[0] + " " + "|" + " " + pos[1] + " | " + pos[2] +  " ");
+
+            if (turn == 'X') {
+                turn = 'O';
+            }
+            else if (turn == 'O') {
+                turn = 'X';
+            }
+        }
     }
 }
