917064d1a1a35c60a5de37701737aac84282fd19
Jkhall81 <jason.kei.hall@gmail.com>
2023-12-05 15:53:37 -0700

    first commit.  Set up command line game board, with board positions linked to an array.  Can now dynamically add X or O to the array and it will appear on the board.

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        char[] pos = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+
+        System.out.println(" " + pos[6] + " " + "|" + " " + pos[7] + " | " + pos[8] +  " ");
+        System.out.println("---+---+---");
+        System.out.println(" " + pos[3] + " " + "|" + " " + pos[4] + " | " + pos[5] +  " ");
+        System.out.println("---+---+---");
+        System.out.println(" " + pos[0] + " " + "|" + " " + pos[1] + " | " + pos[2] +  " ");
+    }
+}
